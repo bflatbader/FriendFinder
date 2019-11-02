ad91c52092f1da6acc3f5785ec77b00b547baa57
Bishop Bader <bflatbader@users.noreply.github.com>
2019-11-01 23:52:58 -0400

    setup express config and html routes

---
+++ server.js
@@ -0,0 +1,20 @@
+// DEPENDENCIES
+var express = require("express");
+var path = require("path");
+
+// EXPRESS CONFIG
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Router
+// require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
